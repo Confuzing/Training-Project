17d6862995e375d085fd2d3505f639d61c472962
Confuzing <simark35@gmail.com>
2016-12-09 12:35:43 -0600

    Added animations to test graphics on lab computers

--- Assets/Scripts/ClickAction.cs
+++ Assets/Scripts/ClickAction.cs
@@ -13,10 +13,24 @@ public class ClickAction : MonoBehaviour {
 			if (didHit) {
 				Debug.Log (rhInfo.collider.name);
 				//rhInfo.collider.gameObject.transform.Rotate (new Vector3 (180, 0, 0));
-				rhInfo.collider.GetComponent<MeshRenderer>().enabled = !(rhInfo.collider.GetComponent<MeshRenderer>().enabled);
+				//rhInfo.collider.GetComponent<MeshRenderer>().enabled = !(rhInfo.collider.GetComponent<MeshRenderer>().enabled);
 				Debug.Log(rhInfo.collider.GetComponent<MeshRenderer>().enabled);
+				StartCoroutine(objectflip (rhInfo));
 			}else
 				Debug.Log ("Update Called");
+		}
+	}
+
+	IEnumerator objectflip (RaycastHit rhInfo)
+	{
+		Vector3 moveDistance = new Vector3 (0f, 0.01f, 0f);
+		for (int i = 1; i <= 50; i++) {
+			rhInfo.collider.gameObject.transform.position += moveDistance;
+			yield return new WaitForSecondsRealtime (0.01f);
+		}
+		for (int i = 1; i <= 50; i++) {
+			yield return new WaitForSecondsRealtime (0.01f);
+			rhInfo.collider.gameObject.transform.position -= moveDistance;
 		}
 	}
 }
